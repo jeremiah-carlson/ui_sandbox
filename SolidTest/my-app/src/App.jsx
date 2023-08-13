62cd0c453786bf15ee541cddd3c989a5b6e85a29
jeremiah-carlson <carlsonjeremiah2@gmail.com>
2023-08-13 00:02:11 -0400

    Add Solid Test

---
+++ SolidTest/my-app/src/App.jsx
@@ -0,0 +1,12 @@
+//import logo from './logo.svg';
+//import styles from './App.module.css';
+
+function App() {
+  return (
+    <div class="bg-slate-500 h-screen w-screen">
+
+    </div>
+  );
+}
+
+export default App;
